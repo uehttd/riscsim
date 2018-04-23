694ebe5ef6dbaf4fe45949f53835002e9c0410fb
yury <manyn2@yandex.ru>
2018-04-23 14:27:49 +0300

    new_

--- test2.c
+++ test2.c
@@ -1,4 +1,14 @@
 //
 // Created by yury Pavlyuk on 23.04.18.
 //
+#include "limits.h"
+#include "stdint.h"
 
+int main(){
+    int32_t a, b, c;
+    a = INT_MAX;
+    b = INT_MAX;
+    c = a + b;
+
+    return 0;
+}
